763d27dbdb49fe1211d98675aee2bef762065072
mike <mike.d.churchill@gmail.com>
2021-04-08 12:27:19 -0400

    thru Chapter 4 mega tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,26 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Michael'}
-    return '''
-<html>
-    <head>
-        <title>Home Page- testpy</title>
-    </head>
-    <body>
-        <h1>Hello,''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'Michael D.'}
+
+    posts = [
+        {
+                'author':{'username': 'John'},
+                'body':'Beautiful day in Clarkston'
+        },
+        {
+                'author':{'username': 'Linda'},
+                'body':'Waiting for The Masters'
+        }
+
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
